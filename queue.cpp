746cd6fec580c5c4c7e5ee788f233f9c918b4e11
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-05-28 14:58:46 +0700

    menambahkan logika untuk menangani pilihan menu pada fungsi main

--- queue.cpp
+++ queue.cpp
@@ -149,3 +149,25 @@ int main()
                 q.insert();
                 break;
             }
+
+            case '2':
+            {
+                q.remove();
+                break;
+            }
+
+            case '3':
+            {
+                q.display();
+                break;
+            }
+
+            case '4':
+            {
+                return 0;
+            }
+
+            
+
+    return 0;
+}
