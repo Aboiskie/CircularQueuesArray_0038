bfb6c86ef08f8f893c6809a28d38c048a46f3300
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-05-28 14:54:50 +0700

    menambahkan metode remove pada kelas Queues

--- queue.cpp
+++ queue.cpp
@@ -44,4 +44,16 @@ public:
                 REAR = REAR + 1;
         }
 
-       
+        queue_array[REAR] = num;
+    }
+
+    void remove()
+    {
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow";
+            return;
+        }
+
+        
