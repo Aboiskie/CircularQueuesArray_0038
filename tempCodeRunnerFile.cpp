eabcd1ef76835590ee6d5356a144053aa3afaf1c
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-05-28 14:59:57 +0700

    menambahkan penanganan untuk opsi menu yang tidak valid dan pengecualian pada fungsi main

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+default:
+            {
+                cout << "Invalid option!!!" << endl;
+                break;
+            }
+            }
+        }
+        catch (exception &ex)
+        {
+            cout << "Check for the values entered." << endl;
+        }
+    }
