c0bdb651a52d9287bc61b1712ae3cb5001de7e55
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-05-28 14:58:23 +0700

    menambahkan logika untuk menangani pilihan menu pada fungsi main

--- queue.cpp
+++ queue.cpp
@@ -142,4 +142,10 @@ int main()
             cin >> ch;
             cout << endl;
 
-            
+            switch (ch)
+            {
+            case '1':
+            {
+                q.insert();
+                break;
+            }
